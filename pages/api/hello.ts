ac494ace0f91e9e7410cfa169708609a04e11e4b
barisorhun <orhun.baris@futurenow.ltd>
2023-03-30 13:57:12 +0300

    nextapi req issue fixing

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,11 +1,10 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { NextRequest } from 'next/server';
 
 export const config = {
 	runtime: 'edge',
 };
 
-export default async function handler(req: NextRequest) {
+export default async function handler(req: NextApiRequest) {
   return new Response(JSON.stringify({ name: 'John Doe' }))
 }
