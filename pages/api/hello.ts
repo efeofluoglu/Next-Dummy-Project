5bb9ed96822cb471a6deaca3984c61a4e73ccd70
barisorhun <orhun.baris@futurenow.ltd>
2023-03-30 13:44:59 +0300

    testing contentful and edgeruntime

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,5 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { NextRequest } from 'next/server';
 
 export const config = {
 	runtime: 'edge',
@@ -8,9 +9,6 @@ type Data = {
   name: string
 }
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default async function handler(req: NextRequest) {
+  return new Response(JSON.stringify({ name: 'John Doe' }))
 }
