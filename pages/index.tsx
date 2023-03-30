e6a1f8904c40ffe457d3dbd93ddc7083457db696
barisorhun <orhun.baris@futurenow.ltd>
2023-03-30 13:59:27 +0300

    still testing edge runtime without getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,34 +2,32 @@ import { Inter } from 'next/font/google'
 import { createClient } from 'contentful';
 import { TeamMember } from '../contentfulTypes';
 
-export async function getStaticProps() {
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID || '',
-    accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
-  });
+// export async function getStaticProps() {
+//   const client = createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID || '',
+//     accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
+//   });
 
-  const res = await client.getEntries<TeamMember>({
-    content_type: 'teamMember',
-  });
+//   const res = await client.getEntries<TeamMember>({
+//     content_type: 'teamMember',
+//   });
 
-  return {
-    props: {
-      members: res.items.map((i) => i.fields),
-    },
-  };
-}
-interface TeamMemberTableProps {
-  members: TeamMember[];
-}
-export default function Home({ members }: TeamMemberTableProps) {
+//   return {
+//     props: {
+//       members: res.items.map((i) => i.fields),
+//     },
+//   };
+// }
+// interface TeamMemberTableProps {
+//   members: TeamMember[];
+// }
+export default function Home() {
 
   
   return (
     <>
       <div>
-        {members.map((member: TeamMember, index: number)=>(
-          <div key={index}>{member.name}</div>
-        ))}
+        hello
       </div>
     </>
   )
